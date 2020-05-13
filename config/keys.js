8d6dea8a886b4415020ab340971686b2ef196794
Vanessa Chen <vanchen07@gmail.com>
2020-05-13 09:45:26 -0700

    update config files

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://vanessa:twittavanessa@cluster0-xy8m8.mongodb.net/twitta?retryWrites=true&w=majority',
-    //Make sure this is your own unique string
-    secretOrKey: "notsecure"
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
